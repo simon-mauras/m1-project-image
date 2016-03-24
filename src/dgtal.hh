b7facd64e38909ea0326993212f3f798db6eaf70
Simon Mauras <simon.mauras@ens-lyon.fr>
2016-03-24 22:54:02 +0100

    Read & Write image

--- src/dgtal.hh
+++ src/dgtal.hh
@@ -3,12 +3,15 @@
 
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
-#include <DGtal/images/ImageContainerBySTLVector.h>
+//#include <DGtal/images/ImageContainerBySTLVector.h>
+#include <DGtal/images/imagesSetsUtils/SetFromImage.h>
+#include <DGtal/images/imagesSetsUtils/ImageFromSet.h>
 #include <DGtal/io/readers/GenericReader.h>
 #include <DGtal/io/writers/GenericWriter.h>
 
 using namespace std;
 using namespace DGtal;
+using namespace Z2i;
 
 typedef ImageContainerBySTLVector<Z2i::Domain, unsigned char> MyImage;
 
